da8e21a1e7331428f9516ef9a8941019f78f4b37
Arjun M S <arjunachu123.aa@gmail.com>
2021-09-18 08:20:47 +0530

    Add function to run Kate

--- main.py
+++ main.py
@@ -6,20 +6,49 @@ kate = sr.Recognizer()
 # this instance is for initializing speech synthesis
 engine = tts.init()
 
-engine.say("I am Kate, your personal assistant")
-engine.say("What can I do for you")
-engine.runAndWait()
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)
 
-try:
-    with sr.Microphone() as source :
-        print("Listening...")
-        voice = kate.listen(source)
-        command = kate.recognize_google(voice).lower()
-        print(command)       
-        if "kate" in command:
-            engine.say("What can I do for you")
-            engine.runAndWait()
-            print("What can I do for you")
-except:
-    pass
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+    # engine.say("I am Kate, your personal assistant")
+    # engine.say("What can I do for you")
 
+def take_command():
+    try:
+        with sr.Microphone() as source :
+            talk("Listening now")
+            print("Listening...")
+            voice = kate.listen(source)
+            command = kate.recognize_google(voice)
+            command = command.lower()
+            print(command)       
+            if "hello" and "kate" in command:
+                print("What can I do for you")
+                talk("What can I do for you")
+                take_command()
+            
+    except:
+        pass
+
+    return command
+
+def listToString(s): 
+    
+    # initialize an empty string
+    str1 = " " 
+    
+    # return string  
+    return (str1.join(s))
+
+def run_kate():
+    command = take_command()
+    if "play" in command:
+        song = command.split(" ")[1:]
+        song = listToString(song)
+        print(song)
+        talk("Playing "+song)
+        print("Playing "+song)
+
+run_kate()
